86fe29f5b4cf7082ef8aa1ab92f46d1cb039372f
minhnguyen98vn <63829855+minhnguyen98vn@users.noreply.github.com>
2020-06-07 12:35:14 -0700

    views.py

--- pythonclubapp/views.py
+++ pythonclubapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import ClubType, Product, Review
 # Create your views here.
 def index(request):
     return render(request, 'pythonclubapp/index.html')
+    
+def getTypes(request):
+    types_list=ClubType.object.all()
+    context={'types_list' : types_list}
+    return render(request, 'ClubReviewApp/types.html', context=context)
     
