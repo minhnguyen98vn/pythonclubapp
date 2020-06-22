9c38b41051da3ee1b15edcecacc07303b377fdb5
minhnguyen98vn <63829855+minhnguyen98vn@users.noreply.github.com>
2020-06-22 11:25:06 -0700

    urls.py

--- pythonclubapp/urls.py
+++ pythonclubapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('getTypes/', views.getTypes, name='types'),
+    path('getProducts/', views.getProducts, name='products'),
+    path('productDetail/<int:id>', views.productDetail, name='productdetail'),
 ]
